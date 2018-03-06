f4f3acb77bf3eb26de3426e8fd2a03ddd7c1fb96
DenimCity <altidorj1@gmail.com>
2018-03-06 15:54:06 -0500

    created a side project data

---
+++ src/sideProjectData.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    name:"",
+    description:"",
+    purpose:"",
+    author:"Wes Bos",
+    github:"",
+    picture:"",
+    demo_link:""
+  },
+  {
+    name:"",
+    description:"",
+    purpose:"",
+    author:"Wes Bos",
+    github:"",
+    picture:"",
+    demo_link:""
+  },
+  {
+    name:"",
+    description:"",
+    purpose:"",
+    author:"Wes Bos",
+    github:"",
+    picture:"",
+    demo_link:""
+  },
+  {
+    name:"",
+    description:"",
+    purpose:"",
+    author:"Wes Bos",
+    github:"",
+    picture:"",
+    demo_link:""
+  },
+]
