86a889bb8c2c846b4f5de7e799b305738f34388b
DenimCity <altidorj1@gmail.com>
2018-03-12 16:24:10 -0400

    refactored component to have images

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,36 +1,41 @@
 import React, {Component} from 'react'
 import styled from 'styled-components'
 import {TitleContainer} from '../styled_components/basicstyles'
+import EnvelopeIcon from './images/002-mail.png'
+import GitHubIcon from './images/001-github-draw-logo.png'
+import TwitterIcon from './images/001-twitter.png'
+import Medium from './images/003-newspaper.png'
+import LinkedIn from './images/004-linkedin.png'
+import UpArrow from './images/001-direction.png'
 export default class Contact extends Component {
   render() {
 
     return (
-      <div id="contact">
+      <ContactContainer id="contact">
         <TitleContainer>
           <h2>Contact</h2>
         </TitleContainer>
         <Grid>
-          
-            <a href="mailto:jeanwaltidor@gmail.com">Email</a>
+            <a href="mailto:jeanwaltidor@gmail.com"><img src={EnvelopeIcon} alt=""/></a>
             <a href="https://github.com/DenimCity" target="blank">
-             GitHub
+             <img src={GitHubIcon} alt=""/>
             </a>
             <a href="https://medium.com/@JW_Altidor" target="blank">
-              Blog
+              <img src={Medium} alt=""/>
             </a>
             <a href="https://twitter.com/JW_Altidor" target="blank">
-              Twitter
+              <img src={TwitterIcon} alt=""/>
             </a>
             <a href="https://www.linkedin.com/in/jeanaltidor" target="blank">
-              LinkedIn
+              <img src={LinkedIn} alt=""/>
             </a>
-
-
-          
         </Grid>
-            <a href="#top">To The Top</a>
+        <ArrowContainer>
+
+            <a href="#top"><img src={UpArrow} alt=""/></a>
+        </ArrowContainer>
          
-      </div>
+      </ContactContainer>
     )
   }
 }
@@ -40,24 +45,34 @@ const Grid = styled.div `
 display:flex;
 flex-wrap:wrap;
 
-justify-content:space-around;
-align-content:center;
-font-size: 3rem;
-@media (max-width: 669px) {
-  display:flex;
-  flex-direction:column;
-  
-  align-items:center;
-  font-size: 3rem;
-  }
+justify-content:center;
+font-size: 3vw;
+
 a {
-  text-decoration: none;
-    color: blue;
-    cursor: pointer;
-    margin: 0.7rem;
-&:hover{
-  color:green;
-}
+  padding-right: 4vw;
+  padding-bottom:4vh;
+  img{
+    width:10vw;
+    @media (max-width: 669px) {
+  width: 15vw;
+  }
+  }
 }
 
 `
+const ContactContainer = styled.div`
+background-color:white;
+`
+
+const ArrowContainer = styled.div`
+display:flex;
+justify-content:center;
+a {
+  img{
+    width:7vw;
+    @media (max-width: 669px) {
+  width:9vw;
+  }
+  }
+}
+` 
