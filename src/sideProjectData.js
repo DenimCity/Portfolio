9daf8dadfd20e4a422a9b78d2b5538cc09087f61
DenimCity <altidorj1@gmail.com>
2018-03-12 16:23:25 -0400

    added description to projects

--- src/sideProjectData.js
+++ src/sideProjectData.js
@@ -1,35 +1,35 @@
 export default [
   {
     name:"Drum Kit",
-    description:"30 Day Vanilla JS Coding Challenge",
+    description:"Making a Drum Kit with Vanilla JS",
     author:"Wes Bos",
     picture:"https://i.imgur.com/BuJYV3x.png",
     link:"http://peaceful-darwin-906e49.bitballoon.com/"
   },
   {
     name:"JS & CSS Clock",
-    description:"30 Day Vanilla JS Coding Challenge",
+    description:"Buidling a CSS clock only using Vanilla JS",
     author:"Wes Bos",
     picture:"https://i.imgur.com/lv6lxAE.png",
     link:"http://stoic-khorana-dd1f6f.bitballoon.com/"
   },
   {
     name:"Flex Panel",
-    description:"30 Day Vanilla JS Coding Challenge",
+    description:"Reactive Image Flex Panel",
     author:"Wes Bos",
     picture:"https://i.imgur.com/zeopICF.jpg",
     link:"http://gracious-pike-0b1033.bitballoon.com/"
   },
   {
     name:"AJax w/ Fetch",
-    description:"30 Day Vanilla JS Coding Challenge",
+    description:"API Calls with Fetch and Vanilla JS",
     author:"Wes Bos",
     picture:"https://i.imgur.com/O41sMYt.png",
     link:"http://epic-euclid-507b3c.bitballoon.com"
   },
   {
     name:"HTML5 Canvas",
-    description:"30 Day Vanilla JS Coding Challenge",
+    description:"Using Html Canvas Element to Paint",
     author:"Wes Bos",
     picture:"https://i.imgur.com/OZIhODf.png",
     link:"http://dazzling-fermat-243fc8.bitballoon.com"
